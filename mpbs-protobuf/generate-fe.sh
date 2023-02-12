943b5d6db26350c1554c1986ab075ff2f8cba98c
karl.taal <karl.taal@helmes.com>
2023-02-12 02:31:08 +0200

    wip

--- mpbs-protobuf/generate-fe.sh
+++ mpbs-protobuf/generate-fe.sh
@@ -4,7 +4,7 @@ PATH_TO_FE_GENERATED='../mpbs-fe/generated'
 
 echo "Setup path"
 FULL_PATH_TO_PROTOC='/opt/homebrew/opt/protobuf@3/bin'
-FULL_PATH_TO_TYPESCRIPT_GENERATOR='/Users/karl.taal/Desktop/photoshare/mpbs-protobuf/node_modules/.bin'
+FULL_PATH_TO_TYPESCRIPT_GENERATOR="$PWD/node_modules/.bin"
 export PATH=$PATH:${FULL_PATH_TO_PROTOC}:${FULL_PATH_TO_TYPESCRIPT_GENERATOR}
 
 echo "Create folder into mpbs-fe"
@@ -13,4 +13,3 @@ mkdir "${PATH_TO_FE_GENERATED}"
 
 echo "Generating files..."
 find ./ -type f -name "*.proto" -exec protoc --ts_out=${PATH_TO_FE_GENERATED} --ts_opt=explicit_override {} \;
-echo "Files generated successfully"
