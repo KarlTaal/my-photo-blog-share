71298329cd659a089b7109e74de081540363dfcf
karl.taal <karl.taal@helmes.com>
2023-02-19 22:35:49 +0200

    be changes test

--- mpbs-be/controller/health/health.handler.go
+++ mpbs-be/controller/health/health.handler.go
@@ -12,5 +12,5 @@ import (
 //	@Success		200	{string}	string "Status comment"
 //	@Router			/healthcheck [get]
 func ServerStatusHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Server is up and running!")
+	return c.JSON(http.StatusOK, "Server is up and running...")
 }
