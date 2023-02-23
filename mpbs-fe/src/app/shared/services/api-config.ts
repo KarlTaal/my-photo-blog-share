a523929643d8c6b16c9e09978c596df675ab5a71
karl.taal <karl.taal@helmes.com>
2023-02-23 15:27:25 +0200

    Angular deploy, dockerfile refacto

--- mpbs-fe/src/app/shared/services/api-config.ts
+++ mpbs-fe/src/app/shared/services/api-config.ts
@@ -1,5 +1,3 @@
-import { environment } from '../../../environments/environment';
-
 export class ApiConfig {
-  readonly baseApiUrl: string = environment.mpbsApiServerUrl + '/mpbs/api';
+  readonly baseApiUrl: string = '/mpbs/api';
 }
--- mpbs-fe/src/environments/environment.ts
+++ mpbs-fe/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
   title: "Localhost environment",
-  mpbsApiServerUrl: ''
 };
